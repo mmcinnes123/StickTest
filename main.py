fecb6bcceee9ae908f5bc730d0cfb5c851f85bf1
mmcinnes123 <m.mcinnnes1.22@abdn.ac.uk>
2023-02-24 08:29:09 +0000

    Added plot

--- main.py
+++ main.py
@@ -46,15 +46,58 @@ with open("MagTest9.csv", 'r') as file:
 Pitch_average_SD = Average(Pitch_SD)
 Roll_average_SD = Average(Roll_SD)
 Yaw_average_SD = Average(Yaw_SD)
+SD = [Pitch_SD, Roll_SD, Yaw_SD]
+
 
 print("The average value of the SD deviation between four sensors for Pitch is: ", str(Pitch_average_SD))
 print("The average value of the SD deviation between four sensors for Roll is: ", str(Roll_average_SD))
 print("The average value of the SD deviation between four sensors for Yaw is: ", str(Yaw_average_SD))
 
+### Create a boxplot of the SDs
+
+fig = plt.figure(figsize=(5, 3))
+ax = fig.add_subplot(111)
+
+# Creating axes instance
+bp = ax.boxplot(SD, patch_artist=True,
+                notch='True', showmeans='True', labels=labels)
+
+colors = ['#0000FF', '#00FF00',
+          '#FFFF00', '#FF00FF']
 
 
+# changing color and linewidth of
+# whiskers
+for whisker in bp['whiskers']:
+    whisker.set(linewidth=1.5)
+
+# changing color and linewidth of
+# caps
+for cap in bp['caps']:
+    cap.set(linewidth=2)
+
+# changing color and linewidth of
+# medians
+for median in bp['medians']:
+    median.set(color='red',
+               linewidth=3)
+
+# changing style of fliers
+for flier in bp['fliers']:
+    flier.set(marker='D',
+              color='#e7298a',
+              alpha=0.5)
+
+# x-axis labels
+ax.set_xticklabels(['Pitch SD', 'Roll SD',
+                    'Yaw SD'])
+
+# Adding title
+plt.title("Standard deviation - Slow")
 
 
+# show plot
+plt.show()
 
 
 
@@ -71,3 +114,4 @@ print("The average value of the SD deviation between four sensors for Yaw is: ",
 # print(new)
 
 # data = np.array(df)
+
